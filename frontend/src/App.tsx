f7ed948f1fe47553ea4e1d8ec2428c3817658eb5
Ana Carolina Kazue Inada <93162137+carol-kazue@users.noreply.github.com>
2023-11-30 10:10:53 +0000

    input estilizado, falta o button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,6 @@ import './App.css';
 import { Button } from './components/Button/Button';
 import  "bootstrap/dist/css/bootstrap.min.css";
 import { Input } from './components/Input/Input';
-import {
-  Routes,
-  Route
-} from "react-router-dom";
-import Books from './pages/Books/Books'
-
 function App() {
   return (
     <div className="App">
--- frontend/src/pages/Books/Books.tsx
+++ frontend/src/pages/Books/Books.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
+import { Button } from '../../components/Button/Button';
+import { Input } from '../../components/Input/Input';
+
+import  "bootstrap/dist/css/bootstrap.min.css";
+
 function Books() {
     return (
       <div className="Books">
-        Sobre
         <br />
-        
+        <Button text='xxx' onClick={()=>{}} color='secondary'></Button>
+      <Input type='livro' id='floatingInput' label="Nome do livro" placeholder='Nome do livro'></Input>
       </div>
     );
   }
